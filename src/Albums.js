079998b11ad15a533a7efad2b2a6cc3c179966e0
MohammadOwais10 <owais0945@gmail.com>
2022-01-19 23:04:15 +0530

    Design of Albums & Artists

--- src/Albums.js
+++ src/Albums.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import albumImage from "../src/assets/images/albumImage.png"
 
 class Albums extends React.Component {
     render() {
         return (
-            <div>
-                <h1>
-                    Albums
-                </h1>
+            <div className="display-album">
+            <img  src={albumImage} alt="Album Image" />
+                <h1>Albums</h1> 
             </div>
         )
     }
--- src/Artists.js
+++ src/Artists.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import artitsImage from "../src/assets/images/artistsImage.jpg"
 
 class Artists extends React.Component {
     render() {
         return (
-            <div>
-                <h1>
-                    Artists
-                </h1>
+            <div className="display-artists">
+            <img  src={artitsImage} alt="Artists Image" />
+                <h1>Artists</h1> 
             </div>
         )
     }
