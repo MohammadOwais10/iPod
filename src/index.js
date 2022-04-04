dbfbd1be81c747c524184918b9e01655d53af563
MohammadOwais10 <owais0945@gmail.com>
2022-04-04 11:30:04 +0530

    Firebase Id update

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
 import firebase from "firebase/app";
 import "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDnbWhOkOAcNSAhy_1hvDBQGmkATIV-E4A",
-  authDomain: "ipod-e4926.firebaseapp.com",
-  projectId: "ipod-e4926",
-  storageBucket: "ipod-e4926.appspot.com",
-  messagingSenderId: "1015279088931",
-  appId: "1:1015279088931:web:5ac479ca79b70a9b108896"
+  apiKey: "AIzaSyCmv6g7bO3e2Jjn9C_phGBuLICeQi_R3DM",
+  authDomain: "ipod-11b25.firebaseapp.com",
+  projectId: "ipod-11b25",
+  storageBucket: "ipod-11b25.appspot.com",
+  messagingSenderId: "671111573331",
+  appId: "1:671111573331:web:c90609e08ce3354c7ebba6",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
